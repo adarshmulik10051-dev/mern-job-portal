02b933e99a363addab0d2162f1bd7055fdc14b29
adarsh mulik <adarshmulik10051@gmail.com>
2026-07-06 22:02:21 +0530

    intial commit

---
+++ backend/utils/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
