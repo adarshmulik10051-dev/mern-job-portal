a3cec60fe351991c6ea829fd5f4925e55af5725d
adarsh mulik <adarshmulik10051@gmail.com>
2026-07-31 00:00:36 +0530

    Add production URL to CORS whitelist

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'],
+    origin: [
+        'http://localhost:5173', 
+        'http://localhost:5174', 
+        'http://localhost:5175', 
+        'http://localhost:5176',
+        'https://mern-job-portal-2wbq.onrender.com'
+    ],
     credentials: true
 }
 
